d884146b2f2266e47e29b20a648f9d253746c09d
Spreco <dinospreco@gmail.com>
2018-09-20 19:52:21 +0200

    Initial commit

---
+++ rest/src/main/java/ba/bildit/javaquizrest/JavaquizrestApplication.java
@@ -0,0 +1,12 @@
+package ba.bildit.javaquizrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaquizrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaquizrestApplication.class, args);
+    }
+}
